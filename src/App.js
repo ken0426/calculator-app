673cf169fc89193c05416e0696b25629cf1241fe
ken0426 <kenkaitixyou0426@gmail.com>
2022-01-04 12:30:25 +0900

    アプリの雛形を作成

--- src/App.js
+++ src/App.js
@@ -1,25 +1,81 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { connect } from "react-redux";
+import {
+    onNumberClick,
+    onPlusClick,
+    onMINUSClick,
+    onMultiplyClick,
+    onDivideClick,
+    onEqualClick,
+    onClearClick,
+} from './redux/actions'
+import Button from "./components/Button";
+import Result from "./components/Result";
+import './App.scss';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const App=(props)=>{
+    const {calculator,
+        onNumberClick,
+        onPlusClick,
+        onMINUSClick,
+        onMultiplyClick,
+        onDivideClick,
+        onEqualClick,
+        onClearClick,
+    }= props;
+    return (
+        <>
+            <div className='result'>
+                <Result result={calculator.showingResult
+                    ? calculator.resultValue
+                    : calculator.inputValue} />
+            </div>
+            <div className='button-wrapper'>
+                <div className='number'>
+                    <div className='upper'>
+                        <Button text={'7'} onClick={()=> onNumberClick(7)} />
+                        <Button text={'8'} onClick={()=> onNumberClick(8)} />
+                        <Button text={'9'} onClick={()=> onNumberClick(9)} />
+                    </div>
+                    <div className='middle'>
+                        <Button text={'4'} onClick={()=> onNumberClick(4)} />
+                        <Button text={'5'} onClick={()=> onNumberClick(5)} />
+                        <Button text={'6'} onClick={()=> onNumberClick(6)} />
+                    </div>
+                    <div className='lower'>
+                        <Button text={'1'} onClick={()=> onNumberClick(1)} />
+                        <Button text={'2'} onClick={()=> onNumberClick(2)} />
+                        <Button text={'3'} onClick={()=> onNumberClick(3)} />
+                    </div>
+                    <div className='bottom'>
+                        <Button text={'0'} onClick={()=> onNumberClick(0)} />
+                        <Button text={'AC'} onClick={()=> onClearClick()} />
+                        <Button text={'='} onClick={()=> onEqualClick()} />
+                    </div>
+                </div>
+                <div className='operator'>
+                        <Button text={'÷'} onClick={()=> onDivideClick()} />
+                        <Button text={'×'} onClick={()=> onMultiplyClick()} />
+                        <Button text={'-'} onClick={()=> onMINUSClick()} />
+                        <Button text={'+'} onClick={()=> onPlusClick()} />
+                    </div>
+            </div>
+        </>
+    );
+};
+
+const mapStateToProps = (state) => {
+    return {
+        calculator: state.calculator,
+    }
 }
 
-export default App;
+export default　connect(mapStateToProps, {
+    onNumberClick,
+    onPlusClick,
+    onMINUSClick,
+    onMultiplyClick,
+    onDivideClick,
+    onEqualClick,
+    onClearClick,
+})(App);
